1b264475e06880792147ecbcb6b0615f3408fa97
Ritu <ritutwinkle10@gmail.com>
2022-12-01 00:52:57 +0530

    depl-check

--- db/connector.js
+++ db/connector.js
@@ -1,13 +1,15 @@
 const mongodb = require("mongodb");
 
 const mongoURI = "mongodb://0.0.0.0:27017/" + "bookMovie";
+const dotenv = require("dotenv");
+dotenv.config();
 
 let mongoose = require("mongoose");
 const { bookMovieSchema } = require("../models/bookMovie");
 const { UserSchema } = require('../models/user')
 
 mongoose
-  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("connection established with mongodb server online");
   })
@@ -19,3 +21,7 @@ let user_connection = mongoose.model("User", UserSchema);
 
 exports.connection = collection_connection;
 exports.Userconnection = user_connection;
+
+//2jldcFIugVIXUpZe
+//RT-Sherlockholmes
+//mongodb+srv://<username>:<password>@cluster0.mgtnz8t.mongodb.net/?retryWrites=true&w=majority
