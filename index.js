125d0e2afa172623acd7f450704c5efcb7b0f284
Ritu <ritutwinkle10@gmail.com>
2022-12-01 16:54:08 +0530

    port changed again!

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 require("dotenv").config();
-const port = process.env.port || 8080;
+const port = 8080;
 const path = require("path");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
